cd78e02ea38a16958cbcfff0e61085b9609d5ef5
notapolloo <madison.howard5323@gmail.com>
2026-02-10 18:37:09 -0500

    Sketch out done most page wips done

---
+++ Full/Public/js/home.js
@@ -0,0 +1,14 @@
+
+  function playAudio() {
+    //if the audio is already playing, mute it
+    var audio = document.getElementById("myAudio");
+
+    if(!audio.paused){
+      audio.pause();
+      audio.currentTime = 0;
+      return;
+    }
+    else
+    audio.play();
+  }
+
